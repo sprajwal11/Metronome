81861895aee2e96165b816fa7536898387d82aea
Prajwal <sharmaprajwal11@gmail.com>
2022-07-04 09:08:29 +0530

    declared variables

---
+++ app.js
@@ -0,0 +1,13 @@
+const tempoDisplay=document.querySelector('.tempo');
+const tempoText=document.querySelector('tempo-text');
+const decreaseTempoBtn=document.querySelector('decrease-tempo');
+const increaseTempoBtn=document.querySelector('increase-tempo');
+const tempoSlider=document.querySelector('slider');
+const startStopBtn=document.querySelector('start-stop');
+const subtractBeats=document.querySelector('subtract-beats');
+const addBeats=document.querySelector('add-beats');
+const measureCount=document.querySelector('measure-count');
+
+
+
+
